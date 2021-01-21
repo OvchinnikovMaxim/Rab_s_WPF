20f4c40a5b66c72bf7224fbed9b7d637e548a0a8
Maxim <maxblackwolfovchinnikov@mail.ru>
2021-01-21 10:47:06 +0300

    add

---
+++ Update_sales_form.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rab_stol
+{
+    /// <summary>
+    /// Логика взаимодействия для Update_sales_form.xaml
+    /// </summary>
+    public partial class Update_sales_form : Window
+    {
+        public Update_sales_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WorkSQL_form.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rab_stol
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkSQL_form.xaml
+    /// </summary>
+    public partial class WorkSQL_form : Window
+    {
+        public WorkSQL_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
