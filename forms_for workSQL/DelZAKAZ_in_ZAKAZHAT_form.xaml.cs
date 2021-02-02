e07159b16310144645b5001ebb78e7049fed485a
Maxim <maxblackwolfovchinnikov@mail.ru>
2021-02-02 10:11:34 +0300

    coding events for del zakaz in zakaz_hat

--- forms_for workSQL/DelZAKAZ_in_ZAKAZHAT_form.xaml.cs
+++ forms_for workSQL/DelZAKAZ_in_ZAKAZHAT_form.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using SQLquerys;
+using System.Data.SqlClient;
 using System.Windows.Shapes;
 
 namespace rab_stol.forms_for_workSQL
@@ -19,9 +21,57 @@ namespace rab_stol.forms_for_workSQL
     /// </summary>
     public partial class DelZAKAZ_in_ZAKAZHAT_form : Window
     {
+        SqlConnection connection;
+
+        Query q = new Query();
+
+        Serv_conn sc = new Serv_conn();
+
         public DelZAKAZ_in_ZAKAZHAT_form()
         {
             InitializeComponent();
+        }
+
+        #region подключение
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            connection = sc.Connection(text_server, label_status);
+        }
+
+        private void text_server_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextServer(e);
+        }
+
+        private void text_server_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sc.TextChanged(connection, text_server, label_status);
+        }
+        #endregion
+
+        private void text_zakazID_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextChisla(e);
+        }
+
+        private void btn_del_zakaz_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlCommand del_zakaz = new SqlCommand(q.del_zakaz_hat(Convert.ToInt32(text_zakazID.Text), comment.Text), connection);
+                del_zakaz.ExecuteNonQuery();
+
+                MessageBox.Show("Заказ удален", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка запроса", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
