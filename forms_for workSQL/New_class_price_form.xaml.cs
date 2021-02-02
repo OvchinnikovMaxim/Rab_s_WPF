2195ce4018c49557f707035c28f90ae458f66ce0
Maxim <maxblackwolfovchinnikov@mail.ru>
2021-02-02 14:25:01 +0300

    add code for event new priceclass

--- forms_for workSQL/New_class_price_form.xaml.cs
+++ forms_for workSQL/New_class_price_form.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SQLquerys;
+using System.Data.SqlClient;
 
 namespace rab_stol.forms_for_workSQL
 {
@@ -19,9 +21,64 @@ namespace rab_stol.forms_for_workSQL
     /// </summary>
     public partial class New_class_price_form : Window
     {
+        SqlConnection connection;
+
+        Query q = new Query();
+
+        Serv_conn sc = new Serv_conn();
+
         public New_class_price_form()
         {
             InitializeComponent();
+
+            combo_class_price.SelectedIndex = 0;
+            combo_zavod.SelectedIndex = 0;
+        }
+
+        #region подключение
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            connection = sc.Connection(text_server, label_status);
+        }
+
+        private void text_server_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextServer(e);
+        }
+
+        private void text_server_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sc.TextChanged(connection, text_server, label_status);
+        }
+        #endregion
+
+        private void btn_class_price_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int zav;
+                int type = 0;
+
+                zav = combo_zavod.SelectedIndex == 0 ? 1 : 2;
+
+                type = combo_class_price.SelectedIndex == 0 ? 20 : 19;
+
+                string name = class_price_name.Text;
+                name = name.Replace("'", "`");
+
+                SqlCommand new_clas_price = new SqlCommand(q.New_priselist_clas(name, type, zav), connection);
+                new_clas_price.ExecuteNonQuery();
+
+                MessageBox.Show("Класс прайс-листа добавлен", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка запроса", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
