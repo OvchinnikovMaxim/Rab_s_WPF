3503160c86fccd9a6e9f82e02251917aa48ab468
Maxim <maxblackwolfovchinnikov@mail.ru>
2021-02-04 13:59:59 +0300

    add code for event new user

--- forms_for workSQL/New_user_inf_form.xaml.cs
+++ forms_for workSQL/New_user_inf_form.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SQLquerys;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace rab_stol.forms_for_workSQL
 {
@@ -19,9 +22,81 @@ namespace rab_stol.forms_for_workSQL
     /// </summary>
     public partial class New_user_inf_form : Window
     {
+        SqlConnection connection;
+
+        readonly Query q = new Query();
+
+        readonly Serv_conn sc = new Serv_conn();
+
+        DataTable dt;
+
+        SqlDataAdapter adapter;
+
         public New_user_inf_form()
         {
             InitializeComponent();
+
+            combo_zavod.SelectedIndex = 0;
+        }
+
+        #region подключение
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            connection = sc.Connection(text_server, label_status);
+        }
+
+        private void text_server_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextServer(e);
+        }
+
+        private void text_server_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sc.TextChanged(connection, text_server, label_status);
+        }
+        #endregion
+
+        private void btn_new_userINF_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int zav = 0;
+                zav = combo_zavod.SelectedIndex == 0 ? 1 : 2;
+
+                SqlCommand new_user_inf = new SqlCommand(q.user_inf_new(text_login.Text, text_fio.Text, text_pass.Text, zav), connection);
+                new_user_inf.ExecuteNonQuery();
+
+                MessageBox.Show("Новый сотрудник добавлен в базу данных", "Результат", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка запроса", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                adapter = new SqlDataAdapter("select * from nefco.dbo.user_inf where user_id like '%" + login_search.Text + "%'", connection);
+
+                dt = new DataTable();
+                adapter.Fill(dt);
+                dataGR.ItemsSource = dt.DefaultView;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка запроса", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
