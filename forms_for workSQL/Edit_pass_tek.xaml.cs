0165669c84189251748662bb2ae6f68eb0cd9686
Maxim <maxblackwolfovchinnikov@mail.ru>
2021-02-05 08:34:57 +0300

    add form for edit pass transp company

---
+++ forms_for workSQL/Edit_pass_tek.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rab_stol.forms_for_workSQL
+{
+    /// <summary>
+    /// Логика взаимодействия для Edit_pass_tek.xaml
+    /// </summary>
+    public partial class Edit_pass_tek : Window
+    {
+        public Edit_pass_tek()
+        {
+            InitializeComponent();
+        }
+    }
+}
