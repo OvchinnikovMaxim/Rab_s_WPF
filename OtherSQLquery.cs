0c4e2aaebb5c574564d0d279bda23849466b1ced
OvchinnikovMaxim <maxblackwolfovchinnikov@mail.ru>
2021-03-30 13:59:24 +0300

    + script for edit tek

--- OtherSQLquery.cs
+++ OtherSQLquery.cs
@@ -174,5 +174,64 @@ namespace rab_stol
 
             return query;
         }
+
+
+        public string Edit_tek(int TEKid,string name, string dogovor, string address, string r_s, string bank, string k_s, string bik, string inn, string kpp, DateTime date_n, int userID)
+        {
+            string query = @"
+DECLARE @name varchar(500) = '" + name + "';" +
+"DECLARE @contract_number varchar(50) = '" + dogovor + "'; " +
+"DECLARE @address varchar(250) = '" + address + "'; " +
+"DECLARE @settlement_account varchar(50) = '" + r_s + "'; " +
+"DECLARE @bank_name varchar(256) = '" + bank + "';" +
+"DECLARE @loro_account varchar(50) = '" + k_s + "'; " +
+"DECLARE @bik varchar(50) = '" + bik + "'; " +
+"DECLARE @inn varchar(50) = '" + inn + "'; " +
+"DECLARE @kpp varchar(50) = '" + kpp + "'; " +
+"DECLARE @contract_date datetime = '" + date_n.ToShortDateString() + "'; " +
+"DECLARE @id INT = " + TEKid + ";" +
+@"
+-- Изменение контрагента
+  UPDATE dbo.co_contractor
+  SET 
+    [name] = @name
+  WHERE
+    id = @id;
+ -- Изменение атрибутов
+ UPDATE
+		dbo.co_contractor_attr_transp
+	  SET
+		contract_number = @contract_number, 
+		address = @address,
+		settlement_account = @settlement_account, 
+		bank_name = @bank_name,
+		loro_account = @loro_account, 
+		bik = @bik,
+		inn = @inn, 
+		kpp = @kpp,
+        contract_date = @contract_date
+	  WHERE
+		contractor_id = @id;
+-- Логирование
+  DECLARE @log_id INT;
+  DECLARE @user_id INT = " + userID + ";" +
+                          @"DECLARE @prgm_id INT = 6;              
+						  DECLARE @tag_id INT = 121;
+  EXEC dbo.co_log_insert @user_id, 6, @tag_id, @id, @log_id OUTPUT;
+
+  DECLARE @log_detail_id INT;
+  EXEC dbo.co_log_detail_insert @log_id, 'наименование', @name, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'Номер договора', @contract_number, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'Адрес', @address, @log_detail_id OUTPUT; 
+  EXEC dbo.co_log_detail_insert @log_id, 'Расчетный счет', @settlement_account, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'имя банка', @bank_name, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'кор. счет', @loro_account, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'бик', @bik, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'инн', @inn, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'кпп', @kpp, @log_detail_id OUTPUT;
+  EXEC dbo.co_log_detail_insert @log_id, 'дата договора', @contract_date, @log_detail_id OUTPUT;";
+
+            return query;
+        }
     }
 }
