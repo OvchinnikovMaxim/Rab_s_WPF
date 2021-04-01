fa23a9895ff329f053828d056cd43199ac88e4f3
OvchinnikovMaxim <maxblackwolfovchinnikov@mail.ru>
2021-04-01 14:02:08 +0300

    add basic code

--- forms_for workSQL/Work_trip.xaml.cs
+++ forms_for workSQL/Work_trip.xaml.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLquerys;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace rab_stol.forms_for_workSQL
 {
@@ -19,9 +15,66 @@ namespace rab_stol.forms_for_workSQL
     /// </summary>
     public partial class Work_trip : Window
     {
+        SqlConnection connection;
+
+        Query q = new Query();
+
+        Serv_conn sc = new Serv_conn();
+
+        DataTable dt;
+
+        SqlDataAdapter adapter;
         public Work_trip()
         {
             InitializeComponent();
         }
+
+        #region подключение
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            connection = sc.Connection(text_server, label_status);
+        }
+
+        private void text_server_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextServer(e);
+        }
+
+        private void text_server_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sc.TextChanged(connection, text_server, label_status);
+        }
+        #endregion
+
+        #region толькочисла
+        private void trip_id_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextServer(e);
+        }
+
+        private void trip_id_history_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            sc.TextServer(e);
+        } 
+        #endregion
+
+        private void trip_id_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void btn_log_trip_1c_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+
+        private void btn_trip_history_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
     }
 }
