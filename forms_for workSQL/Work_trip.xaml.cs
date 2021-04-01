c62923c5fbf3deb27ea6ab9289e0a3c70e076627
OvchinnikovMaxim <maxblackwolfovchinnikov@mail.ru>
2021-04-01 09:48:10 +0300

    add form work for trip

---
+++ forms_for workSQL/Work_trip.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rab_stol.forms_for_workSQL
+{
+    /// <summary>
+    /// Логика взаимодействия для Work_trip.xaml
+    /// </summary>
+    public partial class Work_trip : Window
+    {
+        public Work_trip()
+        {
+            InitializeComponent();
+        }
+    }
+}
